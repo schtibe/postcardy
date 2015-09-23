d0ba8cdd01e1dae0cb77a9bfc992df379e2ead5e
Stefan Heinemann <stefan.heinemann@codedump.ch>
2015-09-23 13:58:37 +0200

    Better file names

--- backend/src/routes/api/v1/image.js
+++ backend/src/routes/api/v1/image.js
@@ -1,7 +1,16 @@
 import { Router } from 'express'
 import multer     from 'multer'
 
-let upload = multer({ dest: 'uploads/' })
+var storage  = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+})
+
+let upload = multer({ storage })
 
 let router = new Router
 export default router
