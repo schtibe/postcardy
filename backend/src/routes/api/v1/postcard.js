674856b04ba3312db3ee36a29be27a1d12142d0e
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-09-17 11:14:35 +0200

    Fixed the postcardcreator

--- backend/src/routes/api/v1/postcard.js
+++ backend/src/routes/api/v1/postcard.js
@@ -1,29 +1,31 @@
-import { Router } from 'express';
-import multer     from 'multer';
-import moment     from 'moment';
-import jsonfile   from 'jsonfile';
+let express = require('express')
+let multer = require('multer')
+let moment = require('moment')
+let jsonfile = require('jsonfile')
+let fs = require('fs')
 
-var fs = require('fs');
+let dataFile = __dirname + '/../../../../config/data.json';
+let configFile = __dirname + '/../../../../config/postconfig.json';
 
-var dataFile = __dirname + '/../../../../config/data.json';
-var configFile = __dirname + '/../../../../config/postconfig.json';
+let config = require(configFile)
 
-var config = require(configFile);
-
-var path = require('path'),
+let path = require('path'),
     Postcardcreator = require('postcardcreator'),
+    SSOPostHelper = require('postcardcreator/lib/helper/SSOPostHelper'),
     Postcard = Postcardcreator.Postcard;
 
-var client = new Postcardcreator(config.username, config.password);
+let SSOHelper = new SSOPostHelper()
 
-let router = new Router;
+
+let router = express.Router();
 export default router;
 
 function isOneDayAgo() {
-  var data = jsonfile.readFileSync(dataFile);
+  return true;
+  let data = jsonfile.readFileSync(dataFile);
 
   if (data.lastOrder) {
-    var lastOrder = moment(data.lastOrder);
+    let lastOrder = moment(data.lastOrder);
 
     if (moment().subtract(1, 'day').isBefore(lastOrder)) {
       return false;
@@ -34,57 +36,72 @@ function isOneDayAgo() {
 }
 
 router.post('/postcards',  (req, res, next) => {
-  var data = jsonfile.readFileSync(dataFile);
+  let data = jsonfile.readFileSync(dataFile);
 
   try {
 
+    /*
     if (data.lastOrder) {
-      var lastOrder = moment(data.lastOrder);
+      let lastOrder = moment(data.lastOrder);
 
       // TODO refactor: use the function maybe
       if (moment().subtract(1, 'day').isBefore(lastOrder)) {
         throw new Error("The last order is less than 24hrs ago (" + lastOrder.format() + ")");
       }
     }
+    */
 
     if (req.body.imgURL == '') {
       throw Error("No image given");
     }
 
-    console.log("Creating postcard");
-    var message = req.body.message;
-    var imgPath = path.join(__dirname, '../../../../', req.body.imgURL);
-    var assetStream = fs.createReadStream(imgPath);
+    SSOHelper.getPostcardcreatorToken(
+      config.username,
+      config.password,
+      function(err, token) {
+        console.log("TOKEN IS" , token)
+        if (err) {
+          console.error("Error getting token", err)
+          throw err
+          return
+        }
 
-    var recipient = {
-      salutation : req.body.salutation,
-      givenName  : req.body.givenName,
-      familyName : req.body.familyName,
-      company    : req.body.company,
-      street     : req.body.street,
-      postCode   : req.body.postCode,
-      place      : req.body.place
-    };
+        let client = new Postcardcreator(token);
 
-    var postcard = new Postcard(assetStream, message, recipient);
+        console.log("Creating postcard");
+        let message = req.body.message;
+        let imgPath = path.join(__dirname, '../../../../', req.body.imgURL);
+        let assetStream = fs.createReadStream(imgPath);
 
-    console.log("Sending postcard")
-    client.sendPostcard(postcard, function(err, result) {
-      if (err) {
-        console.log("Error when sending the postcard. ", err);
-        handleError(err);
-        return;
-      }
+        let recipient = {
+          salutation : req.body.salutation,
+          givenName  : req.body.givenName,
+          familyName : req.body.familyName,
+          company    : req.body.company,
+          street     : req.body.street,
+          postCode   : req.body.postCode,
+          place      : req.body.place
+        };
 
-      console.log("Postcard sent with success !");
-      console.log(result);
-      res.json({ type: 'success', message: 'Sent!' });
-    });
+        let postcard = new Postcard(assetStream, message, recipient);
 
-    var data = jsonfile.readFileSync(dataFile);
-    data.lastOrder = moment();
-    jsonfile.writeFileSync(dataFile, data);
+        console.log("Sending postcard")
+        client.sendPostcard(postcard, function(err, result) {
+          if (err) {
+            console.log("Error when sending the postcard. ", err);
+            //handleError(err);
+            return;
+          }
 
+          console.log("Postcard sent with success !");
+          console.log(result);
+          res.json({ type: 'success', message: 'Sent!' });
+        });
+
+        let data = jsonfile.readFileSync(dataFile);
+        data.lastOrder = moment();
+        jsonfile.writeFileSync(dataFile, data);
+      })
   }
   catch (ex) {
     res.json({ type: 'warning', message: ex.message });
