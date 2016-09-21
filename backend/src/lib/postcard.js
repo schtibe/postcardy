46171c5774ef7b0975adecb2021913f6ca376b2f
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-09-21 22:06:01 +0200

    Refactored the postcard creation code

---
+++ backend/src/lib/postcard.js
@@ -0,0 +1,71 @@
+let Postcardcreator = require('postcardcreator')
+let SSOPostHelper = require('postcardcreator/lib/helper/SSOPostHelper')
+
+let SSOHelper = new SSOPostHelper()
+
+/**
+ * @param {Object} - Response object
+ */
+function _formatResponse(resp) {
+  return `Status: ${resp.status}, Message: ${resp.message}`
+}
+
+/**
+ * @param {string} token - The auth token
+ * @param {readStream} - assetStream The image basically
+ * @param {string} message -The message
+ * @param {Object} callbacks - Callbacks for results
+ * @param {function} callbacks.error - Callback for errors
+ * @param {function} callbacks.success - Callback for success
+ */
+function _sendPostcard(token, recipient, assetStream, message, callbacks) {
+  let client = new Postcardcreator(token)
+  let postcard = new Postcardcreator.Postcard(assetStream, message, recipient)
+
+  client.sendPostcard(postcard, function(err, result) {
+    if (err && 'error' in callbacks) {
+      callbacks.error("Error sending postcard: " +
+          _formatResponse(err))
+    }
+
+    if ('result' in callbacks) {
+      callbacks.result(_formatResponse(result))
+    }
+  })
+}
+
+/**
+ * @param {string} user - Username
+ * @param {string} pw - Password
+ * @param {function} callback - Call when the auth was successful
+ */
+function authorize(user, pw, callback) {
+  SSOHelper.getPostcardcreatorToken(
+    user, pw, (err, token) => {
+      if (err) {
+        if ('error' in callbacks) {
+          callbacks.error('Unable to get token: ' + err)
+        }
+      }
+      callback(token)
+    }
+  )
+}
+
+/**
+ * @param {string} user -Username
+ * @param {string} pw - Password
+ * @param {Object} recipient - Recipient containing the addressing
+ * @param {readStream} - assetStream The image basically
+ * @param {string} message -The message
+ * @param {Object} callbacks - Callbacks for results
+ * @param {function} callbacks.error - Callback for errors
+ * @param {function} callbacks.success - Callback for success
+ */
+function sendPostcard(user, pw, recipient, assetStream, message, callbacks) {
+  authorize(user, pw, (token) => {
+    _sendPostcard(token, recipient, assetStream, message, callbacks)
+  })
+}
+
+export default sendPostcard
--- backend/src/routes/api/v1/postcard.js
+++ backend/src/routes/api/v1/postcard.js
@@ -1,97 +1,76 @@
-import express from 'express'
-import multer from 'multer'
-import moment from 'moment'
-import jsonfile from 'jsonfile'
-import fs from 'fs'
+let express = require('express')
+let multer = require('multer')
+let moment = require('moment')
+let jsonfile = require('jsonfile')
+let fs = require('fs')
+let path = require('path')
+let sendPostcard = require('../../../lib/postcard')
 
 let dataFile = __dirname + '/../../../../config/data.json';
 let configFile = __dirname + '/../../../../config/postconfig.json';
 
 let config = require(configFile)
 
-import path from 'path'
-import { Postcardcreator, Postcard } from 'postcardcreator'
-import SSOPostHelper from 'postcardcreator/lib/helper/SSOPostHelper'
+  let router = express.Router();
+  export default router;
 
-let SSOHelper = new SSOPostHelper()
+  router.post('/postcards',  (req, res, next) => {
+    let data = jsonfile.readFileSync(dataFile);
 
-let router = express.Router();
-export default router;
+    try {
+      /*
+      if (data.lastOrder) {
+        let lastOrder = moment(data.lastOrder);
 
-
-router.post('/postcards',  (req, res, next) => {
-  let data = jsonfile.readFileSync(dataFile);
-
-  try {
-    if (data.lastOrder) {
-      let lastOrder = moment(data.lastOrder);
-
-      // TODO refactor: use the function maybe
-      if (moment().subtract(1, 'day').isBefore(lastOrder)) {
-        throw new Error("The last order is less than 24hrs ago (" + lastOrder.format() + ")");
-      }
-    }
-
-    if (req.body.imgURL == '') {
-      throw Error("No image given");
-    }
-
-    SSOHelper.getPostcardcreatorToken(
-      config.username,
-      config.password,
-      function(err, token) {
-        console.log("TOKEN IS" , token)
-        if (err) {
-          console.error("Error getting token", err)
-          throw err
-          return
+        // TODO refactor: use the function maybe
+        if (moment().subtract(1, 'day').isBefore(lastOrder)) {
+          throw new Error("The last order is less than 24hrs ago (" + lastOrder.format() + ")");
         }
+      }
+      */
 
-        let client = new Postcardcreator(token);
+      if (req.body.imgURL == '') {
+        throw Error("No image given");
+      }
 
-        console.log("Creating postcard");
-        let message = req.body.message;
-        let imgPath = path.join(__dirname, '../../../../', req.body.imgURL);
-        let assetStream = fs.createReadStream(imgPath);
+      let message = req.body.message;
+      let imgPath = path.join(__dirname, '../../../../', req.body.imgURL);
+      let assetStream = fs.createReadStream(imgPath);
 
-        let recipient = {
-          salutation : req.body.salutation,
-          givenName  : req.body.givenName,
-          familyName : req.body.familyName,
-          company    : req.body.company,
-          street     : req.body.street,
-          postCode   : req.body.postCode,
-          place      : req.body.place
-        };
+      let recipient = {
+        salutation : req.body.salutation,
+        givenName  : req.body.givenName,
+        familyName : req.body.familyName,
+        company    : req.body.company,
+        street     : req.body.street,
+        postCode   : req.body.postCode,
+        place      : req.body.place
+      };
 
-        let postcard = new Postcard(assetStream, message, recipient);
-
-        console.log("Sending postcard")
-        client.sendPostcard(postcard, function(err, result) {
-          if (err) {
-            console.log("Error when sending the postcard. ", err);
-            //handleError(err);
-            return;
+      sendPostcard(
+        config.username,
+        config.password,
+        recipient,
+        assetStream,
+        message,
+        {
+          success: function(result) {
+            res.json({ type: 'success', message: 'Sent!' })
+          },
+          error: function(error) {
+            res.json({ type: 'error', message: error})
           }
-
-          console.log("Postcard sent with success !");
-          console.log(result);
-          res.json({ type: 'success', message: 'Sent!' });
-        });
-
-        let data = jsonfile.readFileSync(dataFile);
-        data.lastOrder = moment();
-        jsonfile.writeFileSync(dataFile, data);
-      })
-  }
-  catch (ex) {
-    res.json({ type: 'warning', message: ex.message });
-  }
-});
+        }
+      )
+    }
+    catch (ex) {
+      res.json({ type: 'warning', message: ex.message });
+    }
+  });
 
 
 router.get('/postcards/last',  (req, res, next) => {
-  let isOneDayAgo = True;
+  let isOneDayAgo = true;
 
   let data = jsonfile.readFileSync(dataFile);
 
