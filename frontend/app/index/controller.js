d29a030a602ca99847c211305e5c694bf9f80cf6
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-10-02 19:19:46 +0200

    Started frontend refactoring with flex

--- frontend/app/index/controller.js
+++ frontend/app/index/controller.js
@@ -1,9 +1,20 @@
 import Ember from 'ember'
 
 export default Ember.Controller.extend({
+  uploadProgress: 'none',
+
+  toggleUploadProgress() {
+    if (this.get('uploadProgress') == 'none') {
+      this.set('uploadProgress', )
+    }
+  },
+
   actions: {
-    bubbleImage(chosenPic) {
-      this.set('chosenPic', chosenPic)
+    setImage(image) {
+      this.set('imgURL', image)
+      this.set('imgClass', 'imageSmalle')
+    },
+    uploading() {
     }
   }
 })
