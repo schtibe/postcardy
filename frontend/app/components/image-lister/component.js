e45c0b7e60865a4f1f1c5fda87edaa31ee6d1bb6
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-10-16 16:31:21 +0200

    Refactored the image lister

--- frontend/app/components/image-lister/component.js
+++ frontend/app/components/image-lister/component.js
@@ -1,69 +1,112 @@
 import Ember from 'ember'
-
 import { on, observes } from 'ember-computed-decorators'
 
-/**
- * TODO use
- * https://github.com/ember-cli/ember-ajax
- *
- * oh, and refactor
- */
-
 export default Ember.Component.extend({
+  ajax: Ember.inject.service(),
+
   images: [],
+
+  /**
+   * Specify the range to display
+   */
   from:    0,
   to:      3,
+
+  /**
+   * Whether the up button is enabled
+   */
+  isUpEnabled: false,
+
+  /**
+   * Whether the down button is enabled
+   */
+  isDownEnabled: true,
+
+  /**
+   * Retrieve the available images
+   *
+   * Get a list of available images from
+   * the server as soon as we're loaded
+   */
   @on('didInsertElement')
   @observes('to')
   getImages () {
-    var data = {
+    let data = {
       from: this.get('from'),
       to:   this.get('to')
     }
 
-    Ember.$.ajax({
-      url: '/api/v1/images',
-      type: 'GET',
-      data: data,
-      success: (res) => {
-        this.set('images', res.files)
-        this.set('max',    res.max)
-      }
+    this.get('ajax').request(
+      '/api/v1/images',
+      { data }
+    ).then((res) => {
+      this.set('images', res.files)
+      this.set('max',    res.max)
     })
-
   },
   actions: {
+    /**
+     * When a picture was chosen ('use')
+     * @param {string} image - The image that was chosen
+     * @returns {void}
+     */
     choosePic(image) {
       this.sendAction('choose', image)
     },
+    /**
+     * Up button was clicked. change the range
+     * @returns {void}
+     */
     up() {
       this.set('from' , this.get('from') - 1)
       this.set('to'   , this.get('to')   - 1)
     },
+    /**
+     * Down button was clicked. Shift the range
+     * @returns {void}
+     */
     down() {
       this.set('from' , this.get('from') + 1)
       this.set('to'   , this.get('to')   + 1)
     },
+    /**
+     * Delete an image from the gallery
+     * @param {string} image - The image name
+     * @returns {void}
+     */
     deleteImage(image) {
-      Ember.$.ajax({
-        url: '/api/v1/images',
-        type: 'DELETE',
-        data: { image: image },
-        success: () => {
-          this.getImages()
-          this.disableTo()
-        }
+      this.get('ajax').del(
+        '/api/v1/images',
+        { data: { image } }
+      ).then((res) => {
+        this.getImages()
       })
     }
   },
-  @observes('on')
-  disableTo() {
-    if (this.get('to') + 1 > this.get('max')) {
-      Ember.$('#downBtn').prop('disabled', true)
-    }
-    else {
-      Ember.$('#downBtn').prop('disabled', false)
-    }
+  /**
+   * Determine whether the down button is disabled
+   *
+   * The down button must be disabled if there
+   * are no more images available
+   * @returns{void}
+   */
+  @observes('to')
+  @observes('max')
+  @on('willRender')
+  downState() {
+    const count = 3
+    this.set('isDownEnabled',
+      !(this.get('to') === this.get('max')) &&
+      this.get('max') >= count
+    )
+  },
+  /**
+   * Determine whether the up button is disabled
+   * @returns{void}
+   */
+  @observes('from')
+  upState() {
+    this.set('isUpEnabled', this.get('from') > 0)
   }
 })
 
