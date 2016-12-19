405e79e4c96abab95fef936bba20c73b547e346d
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-12-19 14:28:36 +0100

    Code cleanup, Fixed deprecation warnings

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
-import config from './config/environment';
+import Ember from 'ember'
+import Resolver from 'ember/resolver'
+import loadInitializers from 'ember-load-initializers'
+import config from './config/environment'
 
 var App;
 
@@ -11,8 +11,8 @@ App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
-});
+})
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix)
 
 export default App;
