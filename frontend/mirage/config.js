5b9bbecaa4dec323f07e5f2e57bb2669837ee394
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-12-21 17:11:24 +0100

    Added the postcards/last route to mirage config

--- frontend/mirage/config.js
+++ frontend/mirage/config.js
@@ -24,18 +24,30 @@ export default function() {
     }
   }, { timing: 3000 })
 
-  this.get('/images', (...args) => {
-    return {
-      files: [
+  this.get('/images', (_, request) => {
+    let from = request.queryParams.from
+    let to   = request.queryParams.to
+
+    let files = [
         '/assets/test_images/1.jpg',
         '/assets/test_images/2.jpg',
-        '/assets/test_images/3.jpg'//,
-        //'/assets/test_images/4.jpg'
-      ],
-      max: 4
+        '/assets/test_images/3.jpg',
+        '/assets/test_images/4.jpg'
+      ]
+    return {
+      files: files.slice(from, to),
+      max: files.length
     }
   })
 
+  this.get('/postcards/last', () => {
+    return {
+      data: {
+        date: moment().subtract(1, 'days'),
+        image: '/assets/test_images/4.jpg'
+      }
+    }
+  })
 
   //this.passthrough()
 }
