13de9d99fd3fbdda6335ae6315e67fe5fa5b9799
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-12-23 09:34:19 +0100

    Um, made the tests run again...

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -1,39 +1,27 @@
+/*jshint node:true*/
 /* global require, module */
-
-'use strict'
-
-
-let EmberApp = require('ember-cli/lib/broccoli/ember-app')
+var EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    minifyJS:  false,
-    cssnextOptions: {
-      features: { rem: false },
-      plugins:  [ require('postcss-responsive-type') ],
-      browsers: 'last 2 Firefox versions, last 2 Chrome versions'
-    },
+  var app = new EmberApp(defaults, {
     babel: {
-      includePolyfill: true,
-      optional: [ 'es7.decorators' ],
-      blacklist: [
-        'es3.memberExpressionLiterals',
-        'es3.propertyLiterals',
-        'es5.properties.mutators',
-        'es6.arrowFunctions',
-        'es6.forOf',
-        'es6.spread',
-        'es6.properties.computed',
-        'es6.properties.shorthand',
-        'es6.constants',
-        'es6.templateLiterals'
-      ]
-    },
-    'ember-cli-qunit': {
-        useLintTree: false
+      optional: ['es7.decorators']
     }
-  })
+  });
 
-  return app.toTree()
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  return app.toTree();
 }
-
