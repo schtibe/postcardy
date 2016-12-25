cde150e8f762e5ca493a0d1511424b9de9d6e681
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-12-25 23:46:56 +0100

    Added proper message field test, added missing template file

--- frontend/tests/integration/components/message-field-test.js
+++ frontend/tests/integration/components/message-field-test.js
@@ -1,25 +1,41 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('message-field', 'Integration | Component | message field', {
   integration: true
-});
+})
 
 test('it renders', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  // Set any properties with this.set('myProperty', 'value')
+  // Handle any actions with this.on('myAction', function(val) { ... })
 
-  this.render(hbs`{{message-field}}`);
+  this.render(hbs`{{message-field}}`)
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), '')
 
   // Template block usage:
   this.render(hbs`
     {{#message-field}}
       template block text
     {{/message-field}}
-  `);
+  `)
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+  assert.equal(this.$().text().trim(), 'template block text')
+  assert.equal(this.$('.alert_warning').length, 0)
+})
+
+test('it shows the message after 12 lines', function(assert) {
+  this.render(hbs`{{message-field}}`)
+
+  let messageTmpl = 'Lorem ipsum dolor sit amet aenean suscipit.'
+  let message = ''
+
+  for (let i = 0; i <= 15; i++) {
+    message += `${messageTmpl}\r\n`
+  }
+  this.$('textarea').val(message)
+  this.$('textarea').change()
+
+  assert.ok(this.$('.alert_warning').is(':visible'))
+})
